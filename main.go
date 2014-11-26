31cd4871e0449ffc7acfc1a8f159e47193931c8d
sidharthamani <sidharthamn@gmail.com>
2014-11-26 10:43:18 -0800

    mid way there

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/rancherio/go-rancher/client"
 )
@@ -15,19 +16,23 @@ const (
 	PUT_METHOD    string = "PUT"
 )
 
-type schemaInfo struct {
+type ResourceFieldInfo struct {
 	creatable bool
 	updatable bool
-	deletable bool
-	listable  bool
 }
 
-var schemaInfos map[string]schemaInfo
+type SchemaInfo struct {
+	flagSet            *flag.FlagSet
+	resourceFieldInfos map[string]ResourceFieldInfo
+}
+
+var SchemaInfos map[string]SchemaInfo
 
 func main() {
 	defer func() {
 		str := recover()
 		if str != nil {
+			fmt.Print("ERROR: ")
 			fmt.Println(str)
 		}
 	}()
@@ -35,52 +40,86 @@ func main() {
 	DEFAULT_RANCHER_URL := os.Getenv("RANCHER_URL")
 	DEFAULT_ACCESS_KEY := os.Getenv("RANCHER_ACCESS_KEY")
 
-	rancherUrl := flag.String("url", DEFAULT_RANCHER_URL, "the url of the rancher server")
-	accessKey := flag.String("access-key", DEFAULT_ACCESS_KEY, "the access key for the rancher server")
+	//rancherUrl := flag.String("url", DEFAULT_RANCHER_URL, "the url of the rancher server")
+	//accessKey := flag.String("access-key", DEFAULT_ACCESS_KEY, "the access key for the rancher server")
 
-	flag.Parse()
+	rancherUrl := DEFAULT_RANCHER_URL
+	accessKey := DEFAULT_ACCESS_KEY
 
-	if len(*rancherUrl) == 0 {
+	//flag.Parse()
+
+	if len(rancherUrl) == 0 {
 		panic("RANCHER_URL cannot be empty, please set environment variable RANCHER_URL or use opt --url")
 	}
 
 	opts := new(client.ClientOpts)
-	opts.Url = *rancherUrl
-	if len(*accessKey) > 0 {
-		opts.AccessKey = *accessKey
+	opts.Url = rancherUrl
+	if len(accessKey) > 0 {
+		opts.AccessKey = accessKey
 	}
 	rancherClient, err := client.NewRancherClient(opts)
 	if err != nil {
 		panic(err.Error())
 	}
 
-	//args := os.Args
-
-	schemaInfos = make(map[string]schemaInfo)
+	SchemaInfos = make(map[string]SchemaInfo)
+	args := os.Args
 
 	for _, dataUnit := range rancherClient.Schemas.Data {
 		if dataUnit.Resource.Type != "schema" {
 			continue
 		}
 		id := dataUnit.Resource.Id
-		var typeInfoObj schemaInfo = schemaInfo{false, false, false, false}
+		flagSetForId := flag.NewFlagSet(id, flag.ExitOnError)
 		for _, method := range dataUnit.CollectionMethods {
 			switch method {
 			case GET_METHOD:
-				typeInfoObj.listable = true
 				break
 			case PUT_METHOD:
-				typeInfoObj.updatable = true
+				flagSetForId.Bool("update", false, "update a "+id)
 				break
 			case POST_METHOD:
-				typeInfoObj.creatable = true
+				flagSetForId.Bool("create", false, "create a "+id)
 				break
 			case DELETE_METHOD:
-				typeInfoObj.deletable = true
+				flagSetForId.Bool("delete", false, "delete a "+id)
 				break
 			}
-			schemaInfos[id] = typeInfoObj
+			ResourceFieldInfos := make(map[string]ResourceFieldInfo)
+			for resourceFieldKey := range dataUnit.ResourceFields {
+				resourceFieldValue, ok := dataUnit.ResourceFields[resourceFieldKey]
+				if !ok {
+					continue
+				}
+				ResourceFieldInfos[resourceFieldKey] = ResourceFieldInfo{resourceFieldValue.Create, resourceFieldValue.Update}
+				if id == "agentInstanceProvider" {
+					fmt.Println(resourceFieldKey + ":" + resourceFieldValue.Type)
+				}
+				switch resourceFieldValue.Type {
+				case "string", "date", "blob", "password":
+					flagSetForId.String(resourceFieldKey, "", "set the "+resourceFieldKey)
+					break
+				case "int":
+					flagSetForId.Int(resourceFieldKey, 0, "set the "+resourceFieldKey)
+					break
+				case "float":
+					flagSetForId.Float64(resourceFieldKey, 0.0, "set the "+resourceFieldKey)
+					break
+				case "boolean":
+					flagSetForId.Bool(resourceFieldKey, false, "set the "+resourceFieldKey)
+					break
+				default:
+					if strings.HasPrefix(resourceFieldValue.Type, "reference[") {
+						flagSetForId.String(resourceFieldKey, "", "set the "+resourceFieldKey)
+					}
+					//flagSetForId.Var(resourceFieldValue.Default, resourceFieldKey, "set the "+resourceFieldKey)
+				}
+			}
+			SchemaInfos[id] = SchemaInfo{flagSetForId, ResourceFieldInfos}
 		}
 	}
-	fmt.Println(schemaInfos["container"].listable)
+	// program doesn't reach this block
+	SchemaInfos["agentInstanceProvider"].flagSet.Parse(args[1:])
+	fl := SchemaInfos["agentInstanceProvider"].flagSet.Lookup("accountId")
+	fmt.Println(fl.Value.String())
 }
