f051ddc305b3c851f467b7ec9964cc14b93d2472
James Jeffrey <wizardofmath@gmail.com>
2016-02-16 15:03:04 -0700

    Add policy to context.

--- api/context.go
+++ api/context.go
@@ -19,7 +19,24 @@ type ApiContext struct {
 	UrlBuilder        UrlBuilder
 	apiResponseWriter ApiResponseWriter
 	responseWriter    http.ResponseWriter
+	Policy            Policy
 }
+
+
+type Policy struct {
+	ID interface{} `json:"id"`
+	AccountID int `json:"accountId"`
+	AuthenticatedAsAccountID int `json:"authenticatedAsAccountId"`
+	Identities []Identity `json:"identities"`
+	Username string `json:"username"`
+}
+
+type Identity struct {
+	ExternalID string `json:"externalId"`
+	ExternalIDType string `json:"externalIdType"`
+	ID string `json:"id"`
+}
+
 
 func GetApiContext(r *http.Request) *ApiContext {
 	if rv := context.Get(r, contextKey); rv != nil {
