14b66e9dae0ad7dbd34c055f8489f7c1b3bdab56
Darren Shepherd <darren@rancher.com>
2016-07-27 16:19:09 -0700

    Dapperize

--- catalog/generated_client.go
+++ catalog/generated_client.go
@@ -1,14 +1,14 @@
 package catalog
 
 type RancherClient struct {
-    RancherBaseClient
-	
-    ApiVersion ApiVersionOperations
-    Question QuestionOperations
-    Template TemplateOperations
-    TemplateVersion TemplateVersionOperations
-    Catalog CatalogOperations
-    Error ErrorOperations
+	RancherBaseClient
+
+	ApiVersion      ApiVersionOperations
+	Question        QuestionOperations
+	Template        TemplateOperations
+	TemplateVersion TemplateVersionOperations
+	Catalog         CatalogOperations
+	Error           ErrorOperations
 }
 
 func constructClient() *RancherClient {
@@ -18,25 +18,23 @@ func constructClient() *RancherClient {
 		},
 	}
 
-    
-    client.ApiVersion = newApiVersionClient(client)
-    client.Question = newQuestionClient(client)
-    client.Template = newTemplateClient(client)
-    client.TemplateVersion = newTemplateVersionClient(client)
-    client.Catalog = newCatalogClient(client)
-    client.Error = newErrorClient(client) 
-
+	client.ApiVersion = newApiVersionClient(client)
+	client.Question = newQuestionClient(client)
+	client.Template = newTemplateClient(client)
+	client.TemplateVersion = newTemplateVersionClient(client)
+	client.Catalog = newCatalogClient(client)
+	client.Error = newErrorClient(client)
 
 	return client
 }
 
 func NewRancherClient(opts *ClientOpts) (*RancherClient, error) {
-    client := constructClient()
-        
-    err := setupRancherBaseClient(&client.RancherBaseClient, opts)
-    if err != nil {
-        return nil, err
-    }
+	client := constructClient()
 
-    return client, nil
+	err := setupRancherBaseClient(&client.RancherBaseClient, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
 }
