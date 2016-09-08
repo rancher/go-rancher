943d9ac71ee2f20d098fa39b0fed557bfc8d6b6c
Darren Shepherd <darren@rancher.com>
2016-09-08 12:00:18 -0700

    v2-beta APIs

--- generator/generator.go
+++ generator/generator.go
@@ -88,6 +88,8 @@ func getTypeMap(schema client.Schema) map[string]string {
 			result[fieldName] = "int64"
 		} else if strings.HasPrefix(field.Type, "password") {
 			result[fieldName] = "string"
+		} else if strings.HasPrefix(field.Type, "bool") {
+			result[fieldName] = "bool"
 		} else if _, noConvert := noConversionTypes[field.Type]; noConvert {
 			result[fieldName] = field.Type
 		} else if field.Nullable {
